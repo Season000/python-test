ad65a0acfc4e05dbe9f63bd1ad23b197d945d316
Season000 <43396528+Season000@users.noreply.github.com>
2019-12-15 16:12:40 +0800

    12.15.1

--- 12.15.1.py
+++ 12.15.1.py
@@ -30,6 +30,25 @@ list3 = []
 list3.extend(list2 + list1)
 print(list3)
 
-list = [123,[135,678],234,345] # 列表元素为列表
+list = [123,[135,678],234,345,123] # 列表元素为列表
 print(135 in list[1])
 
+print(list.count(123)) # 计算123在list中出现了多少次
+
+print(list.index(123)) # 列出123元素在列表中第一次出现的索引下标
+print(list.index(123,2,7)) # 在list列表中2-7下标中索引第一次出现的
+
+# reverse  列表翻转
+list = [2,1,3,5,4,6,8,7]
+list.reverse()# 翻转列表，原列表会变化
+print(list)
+
+# sort 列表排序
+list.sort()  # 先从小到大排序，再翻转，就是从大到小排序了
+list.reverse()
+print(list)
+
+# sort(func,key)
+# sort(reverse) 默认为false
+list.sort(reverse=True) # 倒序排序
+print(list)
